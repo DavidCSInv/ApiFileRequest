eff5851ecf53cf20487523bcbb6f0e1c1ac2417e
David Christino <david.silva@supermercadosbh.com.br>
2025-03-27 13:55:08 -0300

    File transfer completed.

--- MoveArquivo/Classes/UploadFile.cs
+++ MoveArquivo/Classes/UploadFile.cs
@@ -1,12 +1,18 @@
-﻿namespace MoveArquivo.Classes
+﻿using Renci.SshNet;
+
+namespace MoveArquivo.Classes
 {
-
-
     public class UploadFile
     {
         public void FileUpload()
         {
+            var host = "your.sftp.host";
+            var port = 22;
+            var username = "your.username";
+            var password = "your.password";
 
+            var client = new SftpClient(host, port, username, password);
+            client.Connect();
         }
     }
 }
--- MoveArquivo/Program.cs
+++ MoveArquivo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using MoveArquivo.Classes;
 
+// Só eu e Deus entendiamos esse codigo, agora só Deus.
 
 #region Parametros de iniciamento
 Render render = new();
@@ -11,79 +12,102 @@ var filePaterns = new Dictionary<int, string>
 };
 
 var directoryOrig = @$"P:";
-
+var directoryCopy = @"C:\Carga\CargaCopia";
 var fileDest = @"C:\Carga\";
-var directoryCopy = @"C:\Carga\Copia";
 
 #endregion
-Render.RenderScreen();
-int option = int.Parse(Console.ReadLine());
 
-#region Option chooser
-switch (option)
+while (true)
 {
-    case 1:
-        render.UploadMinasGerais();
-        break;
-    case 2:
-        render.UploadEspiritoSantos();
-        FileMove(directoryOrig, fileDest, directoryCopy, filePaterns[1], 1);
-        break;
-    case 3:
-        render.UploadMGxES();
-        FileMove(directoryOrig, fileDest, directoryCopy, filePaterns[2], 2);
-        break;
-    case 4:
-        render.UploadESxMG();
-        FileMove(directoryOrig, fileDest, directoryCopy, filePaterns[2], 2);
-        break;
-    default:
+    try
+    {
+        Console.Clear();
         Render.RenderScreen();
-        break;
+        int option = int.Parse(Console.ReadLine());
+        #region Option chooser
+        switch (option)
+        {
+            case 1:
+                render.UploadMinasGerais();
+                string produtos = Console.ReadLine();
+                Thread.Sleep(1000);
+                break;
+            case 2:
+                render.UploadEspiritoSantos();
+                FileMove(directoryOrig, fileDest, directoryCopy, filePaterns[1], 1);
+                break;
+            case 3:
+                render.UploadMGxES();
+                FileMove(directoryOrig, fileDest, directoryCopy, filePaterns[2], 2);
+                break;
+            case 4:
+                render.UploadESxMG();
+                FileMove(directoryOrig, fileDest, directoryCopy, filePaterns[2], 2);
+                break;
+            default:
+                string mensagem = "Favor escolher uma das 4 opções,pressione qualquer tecla para continuar";
+                Console.WriteLine(mensagem);
+                Console.ReadKey();
+                break;
+        }
+        #endregion
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
 }
-#endregion
+
 
 void FileMove(string directoryOrig, string FileDest, string directoryCopy, string filePaterns, int indFile)
 {
     string[] filesGeneric = Directory.GetFiles(directoryOrig, $"{filePaterns}.xlsx", SearchOption.AllDirectories);
     Console.WriteLine("Procurando arquivo...");
 
-    var fileNewPatter = new Dictionary<int, string>
+    string newPatter = "";
+
+    var newFilePatter = new Dictionary<int, string>
     {
        {1,"e11" },
        {2,"tinter01" }
     };
 
+    if (indFile == 1)
+        newPatter = newFilePatter[1];
+    else if (indFile == 2)
+        newPatter = newFilePatter[2];
+
     if (filesGeneric.Length > 0)
     {
         try
         {
-            Console.WriteLine("Arquivo encontrando movendo...");
+            if (!Directory.Exists(directoryCopy))
+            {
+                Directory.CreateDirectory(directoryCopy);
+            }
+
+            Console.WriteLine("Arquivo encontrando, movendo...");
 
             foreach (string fileOrig in filesGeneric)
             {
                 string fileCopy;
+                string fileOriginCopy;
                 int copyNumber = 1;
 
-                File.Copy(fileOrig, fileDest, true);
-                string fileBaseName = fileNewPatter.ContainsKey(1) ? fileNewPatter[1] : fileNewPatter[2]; // Ajuste aqui para garantir que sempre tenha um valor válido
+                fileOriginCopy = Path.Combine(fileDest, $"{newPatter}.xlsx");
 
-                if (!Directory.Exists(directoryCopy))
-                {
-                    Directory.CreateDirectory(directoryCopy);
-                }
+                File.Copy(fileOrig, fileOriginCopy, true);
 
                 do
                 {
-                    fileCopy = Path.Combine(directoryCopy, $"{fileNewPatter}_{copyNumber}.xlsx");
+                    fileCopy = Path.Combine(directoryCopy, $"{newPatter}_{copyNumber}.xlsx");
                     copyNumber++;
                 } while (File.Exists(fileCopy));
 
                 File.Copy(fileOrig, fileCopy);
                 File.Delete(fileOrig);
-                Console.WriteLine("Arquivo ovido com sucesso");
+                Console.WriteLine("Arquivo movido com sucesso");
             }
-
         }
 
         catch (Exception ex)
